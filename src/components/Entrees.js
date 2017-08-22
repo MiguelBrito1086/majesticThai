90353506b76f05b8774cf5634a8212f6ff464520
MiguelBrito1086 <mbrito1086@gmail.com>
2017-08-22 11:31:37 -0400

    working on CSS

---
+++ src/components/Entrees.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+export default class Entrees extends Component {
+
+  render() {
+    // Set items' value....
+    // Since the items do not have a key, let's create one.
+    let entrees = this.props.items.map((item, i) => {
+      // Increment the key for each item.
+      return (
+        <tbody key={i}>
+          <tr>
+            <td>
+              {item.dish}
+            </td>
+            <td>
+              {item.description}
+            </td>
+            <td>
+              {item.price}
+            </td>
+          </tr>
+        </tbody>
+      )
+    })
+    return (
+      <div className="row">
+        <div className="col-md-6 col-md-offset-3">
+          <h4>Entres</h4>
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="col-md-3">Dish</th>
+                <th className="col-md-8">Description</th>
+                <th className="col-md-1">Price</th>
+              </tr>
+            </thead>
+            {entrees}
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
